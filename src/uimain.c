05bbdad2ad20f2938a9d6f7d87e6518342b52c39
araesoawesome <coltseven77@gmail.com>
2021-03-04 15:27:53 -0700

    something

--- src/uimain.c
+++ src/uimain.c
@@ -4,18 +4,21 @@
 #include "history.h"
 
 int main(){
-  while(1){
+  int noexit = 1;
+  while(noexit){
     char input[100];
-    printf(">");
+    printf("> ");
 
     fgets(input, 100, stdin);
 
-    printf("%s", "Input received:", input, "\n", "Tokens:\n");
-
-    
-    char **tokens = tokenize(input);
-    print_tokens(tokens);
-
+    if(*input == '0'){
+      noexit = 0;
+    }
+    else{
+      char** tokens = tokenize(input);
+      print_tokens(tokens);
+      free_tokens(tokens);
+    }
   }
   //past output
   /*printf(space_char(' '));*/
