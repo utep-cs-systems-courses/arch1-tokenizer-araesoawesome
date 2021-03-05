292ef8ea26c31d90ab962904bad074a7ca442a8b
araesoawesome <coltseven77@gmail.com>
2021-03-04 20:29:47 -0700

    fully updated, Segmentation fault?

--- src/uimain.c
+++ src/uimain.c
@@ -5,6 +5,7 @@
 #include "history.h"
 
 int main(){
+  List *history = init_history();
   int noexit = 1;
   while(noexit){
     char input[100];
@@ -15,9 +16,19 @@ int main(){
     if(*input == '0'){
       noexit = 0;
     }
+    else if(*input == 'h'){
+      int i = atoi(input+1);
+      char* his = get_history(history, i);
+      printf("%s","> ", his);
+
+      char** tokenhis = tokenize(his);
+      print_tokens(tokenhis);
+    }
     else{
       char **tokens = tokenize(input);
       print_tokens(tokens);
+      char *sameStr = copy_str(input, strlen(input));
+      add_history(history, sameStr);
       free_tokens(tokens);
     }
   }
